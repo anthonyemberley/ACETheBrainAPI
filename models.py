748de03bab93412fcab0fef3471047aba5a0c34f
Anthony Emberley <emberley@mit.edu>
2016-11-25 12:01:06 -0700

    added unique for username and email

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String())
-    email = db.Column(db.String())
+    username = db.Column(db.String(), unique=True)
+    email = db.Column(db.String(), unique=True)
     password = db.Column(db.String())
 
     def __init__(self, username, email, password):
